978df88a675f9719bf8c26fb2d4e36abc2b9f38e
Qh92 <771600967@qq.com>
2021-07-07 23:52:43 +0800

    feat:添加约瑟夫问题和数组模拟栈的测试代码

--- datastructures/linkedlist/src/com/qinh/Joseph.java
+++ datastructures/linkedlist/src/com/qinh/Joseph.java
@@ -12,6 +12,9 @@ public class Joseph {
         CircleSingleLinkedList list = new CircleSingleLinkedList();
         list.addBoy(5);
         list.showBoy();
+
+        //测试小孩出圈
+        list.countBoy(1,2,5);
     }
 }
 
@@ -72,6 +75,55 @@ class CircleSingleLinkedList{
             //让currentBoy后移
             currentBoy = currentBoy.getNext();
         }
+    }
+
+    /**
+     * 根据用户的输入，计算出小孩出圈的顺序
+     *
+     * @param startNo 表示从第几个小孩开始数数
+     * @param countNum 表示数几下
+     * @param nums 表示最初有多少小孩在圈中
+     */
+    public void countBoy(int startNo,int countNum,int nums){
+        //先对数据进行校验
+        if (first == null || startNo < 1 || startNo > nums){
+            System.out.println("参数输入有误，请重新输入");
+            return;
+        }
+        //创建一个辅助指针，帮助完成小孩出圈
+        Boy helper = first;
+        //需要创建一个辅助指针(变量)helper,事先应该指向环形链表的最后这个节点
+        while (true){
+            //说明helper指向最后一个节点
+            if (helper.getNext() == first){
+                break;
+            }
+            helper = helper.getNext();
+        }
+        //小孩报数前，先让first和helper移动k-1次
+        for (int i = 0; i < startNo -1; i++) {
+            first = first.getNext();
+            helper = helper.getNext();
+        }
+        //当小孩报数时，让first和helper指针同时的移动m-1次，然后出圈
+        //这里是一个循环操作，直到圈中只有一个节点
+        while (true){
+            //说明圈中只有一个节点
+            if (helper == first){
+                break;
+            }
+            //让first和helper指针同时的移动countNum - 1
+            for (int i = 0; i < countNum - 1; i++) {
+                first = first.getNext();
+                helper = helper.getNext();
+            }
+            //这时first指向的节点，就是要出圈的小孩节点
+            System.out.printf("小孩%d出圈\n",first.getNo());
+            //这时将first指向的小孩节点出圈
+            first = first.getNext();
+            helper.setNext(first);
+        }
+        System.out.printf("最后留在圈中的小孩编号%d \n",helper.getNext().getNo());
     }
 
 }
