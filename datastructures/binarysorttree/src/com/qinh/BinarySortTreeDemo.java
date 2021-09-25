60dfa26f322aa0606d9b6fc5f23764d5348fb36e
Qh <771600967@qq.com>
2021-09-25 18:41:25 +0800

    feat:添加二叉排序树测试代码

--- datastructures/binarysorttree/src/com/qinh/BinarySortTreeDemo.java
+++ datastructures/binarysorttree/src/com/qinh/BinarySortTreeDemo.java
@@ -18,9 +18,9 @@ public class BinarySortTreeDemo {
         //二叉排序树的中序遍历是按升序遍历
         sortTree.infixOrder();
 
-        System.out.println("测试删除叶子结点");
-        sortTree.deleteNode(2);
-        sortTree.deleteNode(1);
+        System.out.println("测试删除结点");
+        sortTree.deleteNode(10);
+        //sortTree.deleteNode(1);
         System.out.println("删除结点后");
         sortTree.infixOrder();
 
@@ -111,8 +111,61 @@ class BinarySortTree{
                     parentNode.setRight(null);
                 }
             }
-
+            //删除有两颗子树的结点
+            else if(targetNode.getLeft() != null && targetNode.getRight() != null){
+                int minVal = searchAndDeleteRightMinNode(targetNode.getRight());
+                targetNode.setValue(minVal);
+            }
+            //删除只有一颗子树的结点
+            else {
+                //如果删除的结点有左子结点
+                if (targetNode.getLeft() != null){
+                    if (parentNode != null){
+                        //如果targetNode是父结点的左子结点
+                        if (parentNode.getLeft().getValue() == targetNode.getValue()){
+                            parentNode.setLeft(targetNode.getLeft());
+                        }
+                        //如果targetNode是父结点的右子结点
+                        else if (parentNode.getRight().getValue() == targetNode.getValue()){
+                            parentNode.setRight(targetNode.getLeft());
+                        }
+                    }else {
+                        root = targetNode.getLeft();
+                    }
+                }
+                //如果删除的结点有右子结点
+                else {
+                    if (parentNode != null){
+                        //如果targetNode是父结点的左子结点
+                        if (parentNode.getLeft().getValue() == targetNode.getValue()){
+                            parentNode.setLeft(targetNode.getRight());
+                        }
+                        //如果targetNode是父结点的右子结点
+                        else if (parentNode.getRight().getValue() == targetNode.getValue()){
+                            parentNode.setRight(targetNode.getRight());
+                        }
+                    }else {
+                        root = targetNode.getRight();
+                    }
+                }
+            }
         }
+    }
+
+    /**
+     * 查找以当前删除结点为根结点的右子树的最小结点并删除当前最小结点
+     * @param node 传入的结点（当做二叉排序树的根结点）
+     * @return 返回二叉排序树的右子树中最小的结点
+     */
+    public int searchAndDeleteRightMinNode(Node node){
+        Node target = node;
+        //循环的查找左子结点，就会找到最小的结点
+        while (target != null && target.getLeft() != null){
+            target = target.getLeft();
+        }
+        //target指向最小的结点,并删除该最小结点
+        deleteNode(target.getValue());
+        return target.getValue();
     }
 
 }
