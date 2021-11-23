3557f7d78fb8a0fc4eda3c8cee24a1556eefca94
Qh92 <771600967@qq.com>
2021-11-23 21:40:17 +0800

    feat:LRU算法

--- leetcode/src/main/java/easy/LRUCache.java
+++ leetcode/src/main/java/easy/LRUCache.java
@@ -20,6 +20,9 @@ public class LRUCache {
     int count = 0;
 
     public LRUCache(int capacity) {
+        if (capacity <= 0) {
+            throw new RuntimeException("容量设置异常");
+        }
         this.capacity = capacity;
     }
 
@@ -28,10 +31,11 @@ public class LRUCache {
         if (node != null) {
             //更改指针
             if (node == head) {
-                node.next = head.next;
-                head.next.prev = node;
-                head = node;
-            }else {
+                Node next = head.next;
+                head.next.prev = null;
+                head.next = null;
+                head = next;
+            } else if (node != tail){
                 node.prev.next = node.next;
                 node.next.prev = node.prev;
             }
@@ -48,31 +52,60 @@ public class LRUCache {
     }
 
     public void put(int key, int value) {
-        Node node;
-        if (count < capacity){
-            if (head == null && tail == null) {
-                node = new Node(key,value);
-                head = node;
-                tail = head;
-            }else {
-                node = new Node(key,value);
-                tail.next = node;
-                tail = node.prev;
 
+        Node node;
+        Node containerNode = container.get(key);
+        if (containerNode != null) {
+            node = new Node(key, value);
+            container.put(key, node);
+            if (tail != containerNode) {
+                if (head == containerNode) {
+                    Node next = head.next;
+                    head.next.prev = null;
+                    head.next = null;
+                    head = next;
+                }else {
+                    containerNode.prev.next = containerNode.next;
+                    containerNode.next.prev = containerNode.prev;
+                }
+                tail.next = node;
+                containerNode.prev = tail;
                 tail = node;
             }
-            container.put(key,node);
-            count++;
-        }
-        //移除不常用的数据
-        else {
-            container.remove(head.key);
-            node = new Node(key,value);
-            node.next = head.next;
-            head = node;
-        }
+        }else {
+            if (count < capacity){
+                if (head == null && tail == null) {
+                    node = new Node(key,value);
+                    head = node;
+                    tail = node;
+                }else {
+                    node = new Node(key,value);
+                    tail.next = node;
+                    tail = node.prev;
 
+                    tail = node;
+                }
+                container.put(key,node);
+                count++;
+            }
+            //移除不常用的数据
+            else {
+                container.remove(head.key);
+                node = new Node(key,value);
 
+                Node next = head.next;
+                head.next.prev = null;
+                head.next = null;
+                head = next;
+
+                tail.next = node;
+                node.prev = tail;
+
+                tail = node;
+
+                container.put(key, node);
+            }
+        }
     }
 
 
@@ -89,10 +122,12 @@ public class LRUCache {
     }
 
     public static void main(String[] args){
-        LRUCache lruCache = new LRUCache(2);
+        LRUCache lruCache = new LRUCache(10);
         lruCache.put(1, 1);
         lruCache.put(2, 2);
         lruCache.put(3, 3);
+        lruCache.put(4, 5);
+        lruCache.put(1, 10);
         System.out.println(lruCache.get(1));
         System.out.println(lruCache.get(2));
         System.out.println(lruCache.get(3));
